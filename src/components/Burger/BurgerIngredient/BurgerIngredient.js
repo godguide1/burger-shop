a3489b13e883b6423d2f5cc4f82927bc84ae96ce
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-07-14 21:29:21 +1000

    Reducing Burger Array

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -38,7 +38,7 @@ class BurgerIngredient extends Component {
     }
 } 
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
 
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Auxilliary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
-    render () {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render() {
         return (
             <Auxilliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxilliary>
         );
