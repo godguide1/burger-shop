e19061720e770ccdd54648d2cdf77dfd289773c6
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-07-14 12:47:30 +1000

    Burger Component Hardcoded

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxilliary from '../../hoc/Auxilliary';
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Auxilliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxilliary>
---
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import Auxilliary from '../../hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+    render () {
+        return (
+            <Auxilliary>
+                <Burger />
+                <div>Build Controls</div>
+            </Auxilliary>
+        );
+    }
+}
+
+
+export default BurgerBuilder;
