c362ce90af43132cbb40fd21bd206d97bdb27e39
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-07-15 17:35:36 +1000

    Modal Added to Show Checkout

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Auxilliary from '../../../hoc/Auxilliary';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary =Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize'}}>{igKey}: {props.ingredients[igKey]}</span>
+                </li>
+            )
+        })
+    return (
+        <Auxilliary>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Auxilliary>
+    )
+    
+}
+
+
+export default orderSummary;
