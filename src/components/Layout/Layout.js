78b9349426bee2cdd5871273aaf758228d91be3d
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-07-15 23:18:28 +1000

    Toolbar and SideDrawer Menu Navigation Added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Auxilliary from '../../hoc/Auxilliary';
-import classes from './Layout.css'
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxilliary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxilliary>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render () {
+        return (
+            <Auxilliary>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer 
+                    closed={this.sideDrawerCloseHandler}
+                    open={this.state.showSideDrawer} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxilliary>
+            )
+    }
+
+}
+
+export default Layout;
